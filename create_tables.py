77dca0435c6814148f19f1d23b579d5e0ffa4a22
ehuang68 <>
2021-10-03 23:06:06 -0700

    updated how database table is made

--- create_tables.py
+++ create_tables.py
@@ -7,9 +7,12 @@ create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, userna
 cursor.execute(create_table)
 
 
-create_table = "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name text, price real)"
+create_table = "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name text, price real, store_id integer)"
 cursor.execute(create_table)
 # cursor.execute("INSERT INTO items VALUES ('test', 10.99)")
+
+create_table = "CREATE TABLE IF NOT EXISTS stores (id INTEGER PRIMARY KEY, name text)"
+cursor.execute(create_table)
 
 connection.commit()
 connection.close()
